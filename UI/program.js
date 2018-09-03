90a293c7adfc23784b0e8b1ee1b2862fe46fc05a
Chima, Chibueze <chibbyarena@gmail.com>
2018-09-03 21:36:34 +0100

    feat: Signup and Signin Forms open Order page
    Added program-logon.js to enable the Sign-In and Sign-Up forms to open the Order Page.

--- UI/program.js
+++ UI/program.js
@@ -64,22 +64,7 @@ class ShoppingCart { /* `ShoppingCart` Class beginning */
 } /* `ShoppingCart` Class ending */
 
 
-// `Shop` Subclass extends `ShoppingCart` main Class
-class Shop extends ShoppingCart {
-
-    constructor( total, items, quantity ) {
-        super(total, items);
-
-
-        this.quantity = 100;
-    }
-
-    removeItem() {
-        this.quantity = this.quantity - 1;
-        return this.quantity;
-    }
-}
-//END MAIN CLASS
+// `User` Subclass extends `ShoppingCart` main Class
 
 class User extends ShoppingCart {
 
@@ -101,12 +86,83 @@ class User extends ShoppingCart {
 
 }
 
-const user = new User;
-let list = document.querySelectorAll("ol#ordered-items li");
-let comment = document.querySelector("p.comment");
-let total = document.querySelector("p.total");
-let totalCost = user.total;
+const user = new User; // Creates a new instance of User Object called `user`
 
+
+const comment = document.querySelector("p.comment");
+const total = document.querySelector("p.total");
+let totalCost = 0;
+
+
+
+
+/**
+ * @description Check-Button Function -- Captures and registers `click` events on the `checkbox button`; as well as `submit` event on the `form`.
+ * @param {event} event The click event on `checkboxes`.
+ * @returns {undefined} Sets the `user.total` to the `totalCost`; Activates | deactivates the `Send Order` button; Lists Selected food items.
+ */
+function checkFunc(event) {
+
+  const foodName = event.target.parentNode.parentNode.querySelector(`label`).textContent; // Capture event target, trace and obtain the text content of it's label
+  const foodPriceString = event.target.parentNode.parentNode.querySelector(`figcaption`).getAttribute(`class`); // Capture event target, trace and obtain the `class` of it's `figcaption` (i.e the food item's price)
+  const foodPrice = Number( foodPriceString ); // Converts the price string to a number.
+  const button = document.querySelector('aside button'); // The `Send Order` button -- will be deactivated or activated
+
+  const list = document.querySelectorAll("ol#ordered-items li"); // `NodeList` of list items in the Ordered Food List
+  const input = event.target; // The Event Target
+
+  total.innerHTML = ``; // Clears Total Cost entry once a checkbox is clicked
+
+
+  // What to do when the `input` is `checked` (i.e user had checked the item but wants to uncheck it now) and length of `NodeList` (i.e more than one food items had been selected)
+  if ( input.hasAttribute(`checked`) && list.length > 1 ) {
+
+    comment.innerHTML = `<span class="green">Here is your order list:</span>`; // Displays message that Ordered Food Items is listed
+
+    input.removeAttribute(`checked`); // Unchecks the input
+    user.removeItem(foodName, 1, foodPrice); // Removes the unchecked food from the Order
+
+    let orderID = `#${foodName}`; // Captures ID attribute of the unchecked food
+    let removeOrder = document.querySelector( orderID ); // Captures the unchecked item in the Nodelist
+    removeOrder.remove(); // Removes unchecked food from NodeList
+
+    totalCost = user.total; // Assign updated value of `user.total` to `totalCost` variable
+
+
+  }
+  else if ( list.length === 1 && input.hasAttribute("checked") ) { // NodeList length is 1 and input is already checked because, the input is the only checked item, (and user wants to uncheck it)
+
+    button.setAttribute('class','inactive'); // Deactivate `Send order` button
+
+    comment.innerHTML = `<span>Ordered items will be listed here.</span>`; // Message display that Ordered list would be listed
+
+    input.removeAttribute("checked");
+    user.removeItem(foodName, 1, foodPrice);
+
+    let orderID = `#${foodName}`;
+    let removeOrder = document.querySelector( orderID );
+    removeOrder.remove();
+
+    totalCost = user.total;
+  }
+  else { // This is the first click event on any of the checkboxes since the page loaded
+    comment.innerHTML = `<span class="green">Here is your order list:</span>`; // Displays message that Ordered Food Items is listed
+
+    button.setAttribute('class','active'); // Activates `Send Order` button
+    input.setAttribute("checked", "checked"); // Checks the selected food item
+    user.addItem(foodName, 1, foodPrice); // Adds the food item to the List of ordered food
+
+    const orderList = document.querySelector("ol"); // Obtains the ordered list
+    const newOrder = document.createElement('li'); // Creates a new list item element
+    newOrder.setAttribute('id',foodName); // Sets a unique ID attribute of `foodName` to the list item element
+    newOrder.innerHTML = `<strong>${foodName}</strong>: <span class="pricetag">$${foodPrice}.</span>`; // Sets the unique list item's innerHTML: The foodName and pricetag
+    orderList.appendChild(newOrder); // Appends this unique list item to the ordered list
+    totalCost = user.total;
+
+
+  }
+
+}
 
 /**
  * @description Submit Function -- Captures and registers `click` events on the `button`; as well as `submit` event on the `form`.
@@ -118,54 +174,11 @@ function submitFunc() {
   event.preventDefault(); // Prevents the 'Form' from attempting to submit inputs to a server
 
   if ( totalCost === 0 ) {
-    comment.innerHTML = '<span class="red"> Your have not selected any food item yet</span>';
+    comment.innerHTML = `<span class="red"> Your have not selected any food item yet.</span>`;
     total.innerHTML = ``;
   }
   else if ( totalCost > 0 ) {
-    total.innerHTML = `Total Cost is <strong>$${user.total}</strong>.`;
-  }
-
-}
-
-function checkFunc(event) {
-  let foodName = event.target.parentNode.parentNode.querySelector('label').textContent;
-  let foodPriceString = event.target.parentNode.parentNode.querySelector('figcaption').getAttribute('class');
-  let foodPrice = Number( foodPriceString );
-
-  comment.innerHTML = '<span class="green"> Your have not selected any food item yet</span>';
-
-
-
-  let input = event.target;
-
-  if ( input.hasAttribute("checked") ) {
-    input.removeAttribute("checked");
-    user.removeItem(foodName, 1, foodPrice);
-    totalCost = user.total;
-    return totalCost;
-
-    // console.log( user.total );
-    // console.log( foodName );
-
-  }
-  else {
-    input.setAttribute("checked", "checked");
-
-    user.addItem(foodName, 1, foodPrice);
-
-    const newOrder = document.createElement('li');
-    const orderList = document.querySelector("ol");
-    newOrder.setAttribute('id',foodName);
-    newOrder.innerHTML = `<strong>${foodName}</strong>: <span class="pricetag">$${user.total}</span>`;
-    orderList.appendChild(newOrder);
-    totalCost = user.total;
-    return totalCost;
-
-
-
-    // console.log( user.total );
-
-
+    total.innerHTML = `<span class="green">Total Cost is </span><strong class="pricetag">$${totalCost}</strong>.`;
 
   }
 
